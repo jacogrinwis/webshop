ffd099c8315a22e841b389da70e7b9a85e1f1da7
Jaco Grinwis <jacogrinwis@gmail.com>
2022-12-09 15:04:54 +0100

    added btn styles

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -4,4 +4,46 @@
             Admin Dashboard
         </h2>
     </div>
+    <div class="mb-4">
+        <button class="btn btn-xs btn-primary">Extra small</button>
+        <button class="btn btn-sm btn-primary">Small</button>
+        <button class="btn btn-primary">Base</button>
+        <button class="btn btn-lg btn-primary">Large</button>
+        <button class="btn btn-xl btn-primary">Extra large</button>
+    </div>
+    <div class="mb-4">
+        <button class="btn btn-xs btn-secondary">Extra small</button>
+        <button class="btn btn-sm btn-secondary">Small</button>
+        <button class="btn btn-secondary">Base</button>
+        <button class="btn btn-lg btn-secondary">Large</button>
+        <button class="btn btn-xl btn-secondary">Extra large</button>
+    </div>
+    <div class="mb-4">
+        <button class="btn btn-xs btn-primary btn-outline">Extra small</button>
+        <button class="btn btn-sm btn-primary btn-outline">Small</button>
+        <button class="btn btn-primary btn-outline">Base</button>
+        <button class="btn btn-lg btn-primary btn-outline">Large</button>
+        <button class="btn btn-xl btn-primary btn-outline">Extra large</button>
+    </div>
+    <div class="mb-4">
+        <button class="btn btn-xs btn-secondary btn-outline">Extra small</button>
+        <button class="btn btn-sm btn-secondary btn-outline">Small</button>
+        <button class="btn btn-secondary btn-outline">Base</button>
+        <button class="btn btn-lg btn-secondary btn-outline">Large</button>
+        <button class="btn btn-xl btn-secondary btn-outline">Extra large</button>
+    </div>
+    <div class="mb-4">
+        <button class="btn btn-primary">Primary</button>
+        <button class="btn btn-secondary">Secondary</button>
+        <button class="btn btn-red">Red</button>
+        <button class="btn btn-green">Green</button>
+        <button class="btn btn-yellow">Yellow</button>
+    </div>
+    <div class="mb-4">
+        <button class="btn btn-primary btn-outline">Outline primary</button>
+        <button class="btn btn-secondary btn-outline">Outline secondary</button>
+        <button class="btn btn-red btn-outline">Outline red</button>
+        <button class="btn btn-green btn-outline">Outline green</button>
+        <button class="btn btn-yellow btn-outline">Outline yellow</button>
+    </div>
 </x-admin-layout>
--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -7,7 +7,6 @@
             class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             role="button">Create</a>
     </div>
-
     <div class="overflow-x-auto relative shadow-sm sm:rounded-md mb-6">
         <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
             <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
